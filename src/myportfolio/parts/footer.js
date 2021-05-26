e4915dea6022a5c4a634a180b63275b00645214b
Token-05 <take523oliver@gmail.com>
2021-05-26 23:36:21 +0900

    Update padding

--- src/myportfolio/parts/footer.js
+++ src/myportfolio/parts/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="portfolio">
-            <div className="py-14">©2021, Token, All rights reserved.</div>
+            <div className="py-10">©2021, Token, All rights reserved.</div>
         </footer>
     );
 };
