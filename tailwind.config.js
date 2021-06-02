7f8323a6abe3545d7f955474ede73174ab32f9da
Token-05 <take523oliver@gmail.com>
2021-06-02 22:07:03 +0900

    Update status

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['hover', 'focus'],
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
